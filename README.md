2ce9c38329e0ca1e42b70fc9684431e11cb34066
Zach Arnold <me@zacharnold.org>
2018-10-01 20:41:38 -0700

    update to add some docs and dry-up code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# kubernetes-docs-language-labeler
-The title is the description. :)
+# Kubernetes Docs Language PR Labeler
+Note: I don't have a cool Greek Nautical title for this yet, suggestions are welcome.
+
+## Purpose
+The purpose of the code in this repo is to parse open pull requests against
+the [https://github.com/kubernetes/website](https://github.com/kubernetes/website) repo, and
+assign a label to the PR of which language it believes is responsible for the PR.
+
+## Working with this code
+This repo is based off of the Serverless framework and has its implementation in AWS Lambda. To work with this code,
+you'll need the prerequesites found here: [https://serverless.com/blog/framework-example-golang-lambda-support/](https://serverless.com/blog/framework-example-golang-lambda-support/)
+
+## Roadmap
+With the initial implementation working, the next steps are to:
+
+1) Build a Travis CI CI/CD pipeline so this tool can be updated without the need for @zparnold
+1) Potentially integrate its functionality into Tide from the Kubernetes Test-Infra group
--- processor/main.go
+++ processor/main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"fmt"
 	"context"
-
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-lambda-go/events"
-	"encoding/json"
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
 	"os"
 	"regexp"
 	"strings"
+	"encoding/json"
+
+	"golang.org/x/oauth2"
+	"github.com/google/go-github/github"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/kataras/golog"
+	"github.com/aws/aws-sdk-go/service/kms"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/endpoints"
 )
 
 func main() {
@@ -27,6 +31,7 @@ func ReceieveMessage(ctx context.Context, sqsEvent events.SQSEvent){
 			panic(err)
 		}
 		evt := webhookPayloadData.GetAction()
+		//we only want to label newly opened PRs
 		if evt == "opened"{
 			golog.Info("Calculating delta of files changed")
 			files := getChangedFiles(*webhookPayloadData.Number)
@@ -37,14 +42,8 @@ func ReceieveMessage(ctx context.Context, sqsEvent events.SQSEvent){
 }
 
 func getChangedFiles(prNum int) []*github.CommitFile {
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GH_TOKEN")},
-	)
-	tc := oauth2.NewClient(ctx, ts)
-
-	client := github.NewClient(tc)
-	files, _, err := client.PullRequests.ListFiles(ctx, "zparnold", "website", prNum, &github.ListOptions{})
+	client, ctx := makeGithubClient()
+	files, _, err := client.PullRequests.ListFiles(ctx, os.Getenv("GH_ORG"), os.Getenv("GH_REPO"), prNum, &github.ListOptions{})
 	if err != nil{
 		golog.Fatal(err)
 	}
@@ -53,7 +52,7 @@ func getChangedFiles(prNum int) []*github.CommitFile {
 }
 
 func calculateLanguage(files *[]*github.CommitFile) string {
-	baseUrl := "https://github.com/zparnold/website/blob/"
+	baseUrl := fmt.Sprintf("https://github.com/%s/%s/blob/", os.Getenv("GH_ORG"), os.Getenv("GH_REPO"))
 	re := regexp.MustCompile("\\b[0-9a-f]{5,40}\\b")
 	labelPrefix := "language/"
 	langKeyRef := make(map[string]int)
@@ -67,7 +66,6 @@ func calculateLanguage(files *[]*github.CommitFile) string {
 		basePath := fmt.Sprintf("%s%s/",baseUrl, match[0])
 		blobURL := *v.BlobURL
 		pathUrl := strings.Replace(blobURL, basePath, "", 1)
-		golog.Info("After hacking, this file has path: ", pathUrl)
 		//we care about enumerating values underneath the content directory
 		if strings.HasPrefix(pathUrl, "content") {
 			golog.Info("This file lives in a content/ directory")
@@ -110,15 +108,29 @@ func getAnyKey(m *map[string]int) string{
 
 func applyLanguageLabels(prNum int, label string){
 	golog.Info("Now applying this label to this PR: ", prNum, label)
+	client, ctx := makeGithubClient()
+	_, _, err := client.Issues.AddLabelsToIssue(ctx, os.Getenv("GH_ORG"), os.Getenv("GH_REPO"), prNum, []string{label})
+	if err != nil{
+		os.Exit(1)
+	}
+}
+
+func makeGithubClient() (*github.Client, *context.Context){
+	//Make a pseudo-factory method for producing a client and a context in which GH can execute
+	sess := session.Must(session.NewSession())
+	kmsClient := kms.New(sess, &aws.Config{
+		Region: aws.String(endpoints.UsWest2RegionID),
+	})
+	decryptedOutput, err := kmsClient.Decrypt(&kms.DecryptInput{CiphertextBlob: []byte(os.Getenv("GH_TOKEN"))})
+	if err != nil{
+		golog.Fatal(err)
+	}
 	ctx := context.Background()
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "4b531b237884b2f93ff25d834bbc0fe018e6de68"},
+		&oauth2.Token{AccessToken: string(decryptedOutput.Plaintext)},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 
 	client := github.NewClient(tc)
-	_, _, err := client.Issues.AddLabelsToIssue(ctx, "zparnold", "website", prNum, []string{label})
-	if err != nil{
-		os.Exit(1)
-	}
+	return client, &ctx
 }
